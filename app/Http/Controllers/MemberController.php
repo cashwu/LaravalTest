04436c3d2dd539935bba47ed2e67f837e1630a11
Cash Wu <cash@cashwu.com>
2018-02-11 20:09:32 +0800

    model

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Member;
+
 class MemberController extends Controller
 {
     public function info($id)
     {
-        // blade 
-        return View("member/info", [
-            "name" => "cash"
-        ]);
+        // 使用 Model
+        return Member::getMember();
 
-        // 對應傳統的 php
+        // 使用 blade 
+        //return View("member/info", [
+        //    "name" => "cash"
+        //]);
+
+        // 使用傳統的 php
         //return View("member/memberInfo");
 
         // controller
