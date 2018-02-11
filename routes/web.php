14a8c0f5dca5feb9b240ed8e902952acc9357e28
Cash Wu <cash@cashwu.com>
2018-02-11 18:06:27 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,76 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 基礎路由
+Route::get('basic1', function () {
+   return "Hello World";
+});
+
+Route::get('basic3', function () {
+   return "Hello World 33";
+});
+
+// 需在 VerifyCsrfToken 設置排除路由
+Route::post('basic2', function () {
+    return "basic 2";
+});
+
+// 多請求路由
+// 限定 get , post
+Route::match(['get', 'post'], 'match', function () {
+    return "match";
+});
+
+// 任何
+Route::any('any', function () {
+    return "any";
+});
+
+
+// 路由參數
+//Route::get('user/{id}', function ($id) {
+//    return "User - id - " . $id;
+//});
+
+// 參數可為空
+//Route::get('user/{name?}', function ($name = null) {
+//    return "User - name - " . $name;
+//});
+
+// 參數有預設值
+//Route::get('user/{name?}', function ($name = "cash") {
+//   return "User - name - " . $name; 
+//});
+
+// 使用正則表達式來限定參數
+//Route::get('user/{name?}', function ($name = "cash") {
+//   return "User - name - " . $name; 
+//})->where("name", "[A-Za-z]+");
+
+// 多參數
+//Route::get('user/{id}/{name?}', function ($id, $name = "cash") {
+//   return "User - id - " . $id . " - name - " . $name; 
+//})->where(["id" => "[0-9]+", "name" => "[A-Za-z]+"]);
+
+// 路由別名
+//Route::get('user/member', ["as" => "center" ,function () {
+//   return "member url - " . route("center"); 
+//}]); 
+
+// 路由群組
+Route::group(['prefix' => 'member'], function () {
+    
+    Route::get('center', ["as" => "center" ,function () {
+        return "member url - " . route("center"); 
+    }]); 
+
+    Route::get('admin', function () {
+        return "admin"; 
+    }); 
+});
+
+// 路由輸出 view
+Route::get('view', function () {
+    return view('welcome');
+});
